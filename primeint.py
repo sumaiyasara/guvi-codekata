2d8e6448a0e634a1239a226d547e08a83691669e
sumaiyasara <43085172+sumaiyasara@users.noreply.github.com>
2019-02-07 20:37:39 -0800

    Create primeint.py

---
+++ primeint.py
@@ -0,0 +1,11 @@
+lower,upper = input().split()
+lower= int(lower)
+upper= int(upper)
+  
+for num in range(lower-1,upper):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num)  
