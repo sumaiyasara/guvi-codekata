dd250786cf20d21fc5b3acfe8bf2c29e970eb1c1
sumaiyasara <43085172+sumaiyasara@users.noreply.github.com>
2019-02-10 12:27:06 +0530

    Update removing_palindrome.py

--- removing_palindrome.py
+++ removing_palindrome.py
@@ -1,16 +1,20 @@
-s = input()
-x = []
-for i in range(len(s)):
-    for j in range(i + 1, len(s) + 1):
-        x.append(s[i:j])
-x = sorted(x, reverse=True, key=len)
-for i in x:
-    if i == i[::-1]:
-        temp = i
-        break
-    else:
-        continue
-temp = list(temp)
-s = list(s)
-res = list(set(temp)^set(s))
-print(len(res))
+m=input()
+k=len(m)
+def balin(a):
+    b=len(a)
+    c=[]
+    for i in range(b+1):
+        for j in range(b+1):
+            if(j>i):
+                d=a[i:j]
+                if(d==d[::-1]):
+                    if d not in c:
+                        c.append(d)
+    e=[]
+    for i in c:
+        e.append(len(i))
+    f=max(e)
+    g=e.index(f)
+    h=c[g]
+    return(len(h))
+print(k-balin(m))
