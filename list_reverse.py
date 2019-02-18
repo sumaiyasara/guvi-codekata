cfd46b5180d80bd582967090e224b03b31f7f6f3
sumaiyasara <43085172+sumaiyasara@users.noreply.github.com>
2019-02-18 14:37:29 +0530

    Update list_reverse.py

--- list_reverse.py
+++ list_reverse.py
@@ -1,10 +1,10 @@
 while True:
 	try:
 		x=int(input())
-		a=input().split()
+		k=input().split()
 		break
 	except:
 		print("Invalid input")
 		break
-if(len(a)==x):
-	print('->'.join(str(x) for x in a[::-1]))
+if(len(k)==x):
+	print('->'.join(str(x) for x in k[::-1]))
